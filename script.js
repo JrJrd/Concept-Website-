74f021c5d020faabb21fd1e77d10aa9667b5cf75
atSeaer <121316243+atSeaer@users.noreply.github.com>
2023-05-09 17:12:45 +0300

    Video/Animation
    Added "about-us" section and video.
    Added JS function to play/pause the video.
    Added JS function for the bar width during video play time
    Added JS function for the main menu drop down arrow.

---
+++ script.js
@@ -0,0 +1,43 @@
+// navbar
+const menu = document.querySelector('.menu')
+const navbar = document.querySelector('.navbar')
+
+menu.addEventListener('click', () => {
+    navbar.classList.toggle('change')
+    menu.classList.toggle('change')
+
+    })
+//end of navbar
+
+//section 2 vid
+const video = document.querySelector('.video')
+const btn = document.querySelector('.buttons i')
+const bar = document.querySelector('.video-bar')
+
+const playPause = () => {
+    if(video.paused) {
+        video.play()
+        btn.className = 'far fa-pause-circle'  
+        video.style.opacity = '1.7'
+    } else {
+        video.pause()
+        btn.className = 'far fa-play-circle'
+        video.style.opacity = '0.3'
+    }
+
+}
+
+btn.addEventListener('click', ()=> {
+    playPause()
+})
+
+
+video.addEventListener('timeupdate', () => {
+    const barWidth = video.currentTime/video.duration
+    bar.style.width = `${barWidth * 100}%`
+    if(video.ended) {
+        btn.className = 'far fa-play-circle'
+        video.style.opacity = "0.3"
+    }
+})
+//end section 2 vid
