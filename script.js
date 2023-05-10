a233d55bc329570dade48201cd372b7637f1fad6
atSeaer <121316243+atSeaer@users.noreply.github.com>
2023-05-10 16:58:08 +0300

    Swipe plugin
    Added pricing cards with swipe bundle JS plugin for swipe animation for the now finished pricing section

--- script.js
+++ script.js
@@ -1,43 +1,56 @@
-// navbar
-const menu = document.querySelector('.menu')
-const navbar = document.querySelector('.navbar')
+// Navbar
+const menu = document.querySelector(".menu");
+const navbar = document.querySelector(".navbar");
 
-menu.addEventListener('click', () => {
-    navbar.classList.toggle('change')
-    menu.classList.toggle('change')
+menu.addEventListener("click", () => {
+  navbar.classList.toggle("change");
+  menu.classList.toggle('change')
+});
+// End of Navbar
 
-    })
-//end of navbar
-
-//section 2 vid
+// Section 2 Video
 const video = document.querySelector('.video')
-const btn = document.querySelector('.buttons i')
+const btn = document.querySelector('.buttons button i');
 const bar = document.querySelector('.video-bar')
 
 const playPause = () => {
-    if(video.paused) {
-        video.play()
-        btn.className = 'far fa-pause-circle'  
-        video.style.opacity = '1.7'
-    } else {
-        video.pause()
-        btn.className = 'far fa-play-circle'
-        video.style.opacity = '0.3'
-    }
-
+  if(video.paused) {
+    video.play()
+    btn.className = 'far fa-pause-circle'
+    video.style.opacity = '.7'
+  } else {
+    video.pause()
+    btn.className = 'far fa-play-circle'
+    video.style.opacity = '.3'
+  }
 }
 
-btn.addEventListener('click', ()=> {
-    playPause()
+btn.addEventListener('click', () => {
+  playPause()
 })
 
 
 video.addEventListener('timeupdate', () => {
-    const barWidth = video.currentTime/video.duration
-    bar.style.width = `${barWidth * 100}%`
-    if(video.ended) {
-        btn.className = 'far fa-play-circle'
-        video.style.opacity = "0.3"
-    }
+  console.log(video.currentTime, video.duration)
+  const barWidth = video.currentTime / video.duration;
+  bar.style.width = `${barWidth * 100}%` 
+  if(video.ended) {
+    btn.className = 'far fa-play-circle'
+    video.style.opacity = '.3'
+  } 
 })
-//end section 2 vid
+// End of Section 2 Video
+
+var swiper = new Swiper('.swiper-container', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 70,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  }
+});
